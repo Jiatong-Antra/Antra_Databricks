61e8bbe62480b274ff04774f94e6ce9132818448
Jiatong-Antra <Jiatong-Antra>
2021-12-22 07:02:32 +0000

    added print test

--- classic/00_ingest_raw.py
+++ classic/00_ingest_raw.py
@@ -161,3 +161,7 @@ print(
 # MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
 # MAGIC <br/>
 # MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="http://help.databricks.com/">Support</a>
+
+# COMMAND ----------
+
+print("test")
